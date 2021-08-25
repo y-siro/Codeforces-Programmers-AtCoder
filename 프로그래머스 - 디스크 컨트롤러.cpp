d314c1b268f5bfca5fe12ba99e936ce26fd5a211
y-siro <82633123+y-siro@users.noreply.github.com>
2021-08-26 02:13:52 +0900

    pq (or vector)

---
+++ 프로그래머스 - 디스크 컨트롤러.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int m_f = 0, cnt = 0;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+int solution(vector<vector<int>> jobs) { 
+    int answer = 0;
+    sort(jobs.begin(), jobs.end());
+    while (!q.empty() || cnt < jobs.size()) {
+        if (m_f >= jobs[cnt][0] && cnt < jobs.size()) {
+            q.push(make_pair(jobs[cnt][1], jobs[cnt][0]));
+            cnt++;
+            continue;
+        }
+
+        else {
+            if (!q.empty()) {
+                m_f += q.top().first;
+                answer += m_f - q.top().second;
+                q.pop();
+            }
+            else {
+                m_f = jobs[cnt][0];
+            }
+        }
+    }
+
+    answer /= jobs.size();
+    return answer;
+}
+
+int main(void) {
+    // input func
+    // 
+    return 0;
+}
